f39047894aa927b816a08cba01c9f7b31b263a8c
mihaupt <mihaupt@ebay.com>
2020-06-16 18:05:21 +0200

    print is a function

--- trepic.py
+++ trepic.py
@@ -9,22 +9,22 @@ LOG = True
 # process
 
 if len(sys.argv) < 2:
-    print 'Please pass the name of the new epic.'
+    print('Please pass the name of the new epic.')
     sys.exit(1)
 
 epic_name = ' '.join(sys.argv[1:])
 
-if LOG: print 'Creating new epic board ...'
+if LOG: print('Creating new epic board ...')
 (epic_board_id, epic_board_url) = copy_board(epic_name, TEMPLATE_BOARD_ID)
 
-if LOG: print 'Adding epic to main board ...'
+if LOG: print('Adding epic to main board ...')
 epic_ticket_id = new_ticket(PRIMORDIAL_SOUP_LIST_ID, epic_name)
 
-if LOG: print 'Labelling ticket as epic ...'
+if LOG: print('Labelling ticket as epic ...')
 label_ticket(epic_ticket_id, EPIC_LABEL_ID)
 
-if LOG: print 'Linking epic ticket and board ...'
+if LOG: print('Linking epic ticket and board ...')
 add_attachment(epic_ticket_id, epic_board_url)
 
-if LOG: print 'Done.'
+if LOG: print('Done.')
 
